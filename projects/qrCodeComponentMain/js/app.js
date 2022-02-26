6a28af50b58cfdd7587fca66eccf8b05b938d8d6
Esteban Jiménez Ruiz <estebanj659@gmail.com>
2022-02-26 00:50:52 -0600

    corrección del atributo alt los tag main

--- projects/qrCodeComponentMain/js/app.js
+++ projects/qrCodeComponentMain/js/app.js
@@ -1,5 +1,6 @@
 let qrCode = new QRCode( document.querySelector(".qrCode"), {
     text: "https://www.frontendmentor.io/",
+    alt: "Código QR que redirige hacia frontendmentor",
     width: 180,
     height: 180,
     colorDark: "#FFFFFF",
